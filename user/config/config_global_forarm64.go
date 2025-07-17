e094c6fb4f524a36facd0cdeba048730db23ac3b
CrazyZhang <917724035@qq.com>
2025-07-17 12:27:56 +0800

    Update config_global_forarm64.go

--- user/config/config_global_forarm64.go
+++ user/config/config_global_forarm64.go
@@ -8,7 +8,7 @@ func (this *GlobalConfig) Is32Bit() bool {
 }
 
 func (this *GlobalConfig) GetSyscallConfigFile() string {
-    return "user/config/config_syscall_aarch64.json"
+    return "/sdcard/user/config/config_syscall_aarch64.json"
 }
 
 func (this *GlobalConfig) InitLibraryDirs() {
